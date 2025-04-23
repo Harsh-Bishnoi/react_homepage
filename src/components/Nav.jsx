c191b2c0b363ca5e8c2f23e3a74788f07be641e3
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-04-23 12:26:20 +0530

    nav done

---
+++ src/components/Nav.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react';
+import logo from '../assets/images/svg/nav-logo.svg';
+
+const Nav = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleNavbar = () => {
+        setMenuOpen(!menuOpen);
+        document.body.classList.toggle("overflow-hidden", !menuOpen);
+    };
+
+    useEffect(() => {
+        return () => {
+            document.body.classList.remove("overflow-hidden");
+        };
+    }, []);
+
+    return (
+        <nav className='bg-black'>
+            <div className="max-w-[1320px] px-4 mx-auto">
+                <div className="flex justify-between items-center py-[17px]">
+                    <div className="flex items-center gap-3.5">
+                        <a href="#"><img src={logo} alt="nav-logo" className='max-w-[50px] sm:max-w-[98px]' /></a>
+                        <a className='font-family-primary font-bold text-2xl sm:text-4xl leading-[135%] text-[#2C49FE] mb-0' href='#'>Events<span className='text-[#01C8FF]'>Free</span></a>
+                    </div>
+                    <div onClick={toggleNavbar} className="z-50 lg:hidden flex flex-col justify-between w-6 h-5 cursor-pointer">
+                        <span className={`block h-1 rounded-3xl bg-white ${menuOpen ? 'rotate-45 translate-y-2' : ''}`}></span>
+                        <span className={`block h-1 rounded-3xl bg-white ${menuOpen ? 'opacity-0' : ''}`}></span>
+                        <span className={`block h-1 rounded-3xl bg-white ${menuOpen ? '-rotate-42 -translate-y-2' : ''}`}></span>
+                    </div>
+                    <ul id='nav-name' className={`list-unstyled flex justify-center items-center nav-link gap-[60px] mb-0 ${menuOpen ? "show-navbar" : ""}`}>
+                        <li><a className='font-family-primary font-normal text-base leading-[100%] text-white hover:font-semibold' href="#">HOME</a></li>
+                        <li><a className='font-family-primary font-normal text-base leading-[100%] text-white hover:font-semibold' href="#">EVENTS</a></li>
+                        <li><a className='font-family-primary font-normal text-base leading-[100%] text-white hover:font-semibold' href="#">FEED</a></li>
+                        <li><a className='font-family-primary font-normal text-base leading-[100%] text-white hover:font-semibold' href="#">USER NAME</a></li>
+                        <li><button className='font-family-primary font-semibold text-base leading-[150%] border-0 bg-[#2C49FE] rounded-3xl py-[11px] px-[28px] text-white cursor-pointer hover:bg-[white] hover:text-[#01C8FF] transition-all duration-200 ease-linear'>Log Out</button></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Nav;
