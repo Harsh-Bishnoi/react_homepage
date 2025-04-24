40c7d2016246577080d0f29bb7f6875cb93e3d21
Harsh-Bishnoi <harshbishnoi855@gmail.com>
2025-04-24 16:48:33 +0530

    add transition on cross

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -25,9 +25,9 @@ const Nav = () => {
                         <a className='font-family-primary font-bold text-2xl sm:text-4xl leading-[135%] text-[#2C49FE] mb-0' href='#'>Events<span className='text-[#01C8FF]'>Free</span></a>
                     </div>
                     <div onClick={toggleNavbar} className="z-50 lg:hidden flex flex-col justify-between w-6 h-5 cursor-pointer">
-                        <span className={`block h-1 rounded-3xl bg-white ${menuOpen ? 'rotate-45 translate-y-2' : ''}`}></span>
-                        <span className={`block h-1 rounded-3xl bg-white ${menuOpen ? 'opacity-0' : ''}`}></span>
-                        <span className={`block h-1 rounded-3xl bg-white ${menuOpen ? '-rotate-42 -translate-y-2' : ''}`}></span>
+                        <span className={`block h-1 rounded-3xl bg-white transition-transform duration-300 ease-in-out ${menuOpen && 'rotate-45 translate-y-2'}`}></span>
+                        <span className={`block h-1 rounded-3xl bg-white transition-transform duration-300 ease-in-out ${menuOpen && 'opacity-0'}`}></span>
+                        <span className={`block h-1 rounded-3xl bg-white transition-transform duration-300 ease-in-out ${menuOpen && '-rotate-45 -translate-y-2'}`}></span>
                     </div>
                     <ul id='nav-name' className={`list-unstyled flex justify-center items-center nav-link gap-[60px] mb-0 ${menuOpen ? "show-navbar" : ""}`}>
                         <li><a className='font-family-primary font-normal text-base leading-[100%] text-white hover:font-semibold' href="#">HOME</a></li>
